48697945974d3c3f771910c70d0d7d5b6c9c124a
Senih Berkay Akın <99797076+senihberkay@users.noreply.github.com>
2022-09-21 21:15:21 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,11 @@
 lst = [
     ["https://sabanciuniv.zoom.us/j/96959114776?pwd=OER5MTdSZFhFT3dyVkhtcFJRdTZSQT09", "19:54", "23:00"]
 ]
+
+
+"""
+lst = [
+    ["zoom_link_1", "19:54", "23:00"],
+    ["zoom_link_2", "08:37", "10:32"]
+]
+"""
