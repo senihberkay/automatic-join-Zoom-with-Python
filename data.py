feb4cf986bfd64d0fea9812e78fc631d52b5a60a
Senih Berkay Akın <99797076+senihberkay@users.noreply.github.com>
2022-09-21 21:16:10 +0300

    Update data.py

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ lst = [
     ["https://sabanciuniv.zoom.us/j/96959114776?pwd=OER5MTdSZFhFT3dyVkhtcFJRdTZSQT09", "19:54", "23:00"]
 ]
 
-
+# As you wish you can add more than one Zoom meeting links
 """
 lst = [
     ["zoom_link_1", "19:54", "23:00"],
