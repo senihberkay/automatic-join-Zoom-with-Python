b4fadfef874c7b2e8faa8a51c5dc9fd769ec7c86
Senih Berkay Akın <99797076+senihberkay@users.noreply.github.com>
2022-09-21 21:12:25 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,3 @@
 lst = [
-    ["https://us06web.zoom.us/j/85362911867?pwd=ZWczck5HZERGaE1VOTBNM3FOT1dTQT09", "19:54", "23:00"]
+    ["https://sabanciuniv.zoom.us/j/96959114776?pwd=OER5MTdSZFhFT3dyVkhtcFJRdTZSQT09", "19:54", "23:00"]
 ]
